4d3888bbf9b091c17c21cda1d64e8eb46f040f8b
Jewel Mia <145703213+jewel1269@users.noreply.github.com>
2024-05-11 18:33:43 +0600

    delete functionality complete

--- index.js
+++ index.js
@@ -33,6 +33,9 @@ async function run() {
     const database = client.db("assignmentDB");
     const assignmentCollection = database.collection("allAssignment");
 
+    const databaseTwo = client.db("submittedAssignment");
+    const submittedAssignment = databaseTwo.collection("SubmittedA.");
+
 
 
 app.post("/assignments", async(req, res)=>{
@@ -43,16 +46,66 @@ app.post("/assignments", async(req, res)=>{
   
 })
 
+app.post("/submittedAssignment", async(req, res)=>{
+  const item = req.body;
+  const result = await submittedAssignment.insertOne(item);
+  res.send(result)
+  console.log(result);
+  
+})
+
 app.get("/assignments", async(req, res)=>{
   const result = await assignmentCollection.find().toArray();
   res.send(result)
 })
 
-app.get("/products", async(req, res)=>{
-  const cursor = userCollection.find();
-  const result = await cursor.toArray();
-  res.send(result);
+app.get("/submittedAssignment", async(req, res)=>{
+  const result = await submittedAssignment.find().toArray();
+  res.send(result)
 })
+
+
+app.get("/assignments/:email", async (req, res) => {
+  console.log(req.params.email)
+  const result = await assignmentCollection.find({email: req.params.email}).toArray();
+  res.send(result);
+});
+
+app.delete("/assignments/:email", async (req, res) => {
+  const email = req.params.email;
+  console.log(email)
+  const query = { email: email };
+  const result = await assignmentCollection.deleteOne(query);
+  res.send(result);
+});
+// app.put("/countries/:id", async(req, res)=>{
+//   const id = req.params.id
+//   const filter= { _id: new ObjectId(id)}
+//   const options = {upsert: true};
+//   const updateData = req.body;
+//   console.log(updateData)
+//   const updateCountries ={
+//     $set:{
+//       name:updateData.name,
+//       userEmail:updateData.userEmail,
+//       photoURL:updateData.photoURL,
+//       spot_name:updateData.spot_name,
+//       country_name:updateData.country_name,
+//       price:updateData.price,
+//       season:updateData.season,
+//       travelTime:updateData.travelTime,
+//       visitor:updateData.visitor,
+//       description:updateData.description,
+//     }
+    
+//   }
+//   const result = await countryCollection.updateOne(filter, updateCountries, options);
+//   console.log(result)
+//   res.send(result)
+  
+// })
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
