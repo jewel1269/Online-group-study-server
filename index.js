ddbf1f1239044563f69b2ef7d8b894a7bbccd482
Jewel Mia <145703213+jewel1269@users.noreply.github.com>
2024-05-14 15:51:41 +0600

    jwt half complete

--- index.js
+++ index.js
@@ -10,11 +10,21 @@ const port = process.env.PORT || 5000;
 
 
 app.use(express.json())
-app.use(cors())
+app.use(cookieParser())
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174"
+    ],
+    credentials: true,
+  })
+);
 
 console.log(process.env.DB_USER);
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { populate } = require('dotenv');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0supnyg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,6 +35,28 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+const logger = (req, res, next) => {
+  console.log("Called", req.hostname, req.originalUrl);
+  next();
+};
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log(token);
+  if (!token) {
+    return res.status(401).send({ message: 'Not Authorized' });
+  }
+jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+  if(err){
+    console.log(err);
+    return res.status(401).send({ message: 'Not Authorized' });
+  }
+  console.log('value in the', decoded);
+  req.user = decoded;
+  next();
+})
+};
 
 async function run() {
   try {
@@ -56,20 +88,20 @@ app.post("/submittedAssignment", async(req, res)=>{
 
 
 
-app.get("/assignments", async(req, res)=>{
+app.get("/assignments", logger, verifyToken, async(req, res)=>{
   const result = await assignmentCollection.find().toArray();
   res.send(result)
 })
 
 
 
-app.get("/submittedAssignment", async(req, res)=>{
+app.get("/submittedAssignment", logger, verifyToken, async(req, res)=>{
   const result = await submittedAssignment.find().toArray();
   res.send(result)
 })
 
 
-app.get("/submittedAssignment/:email", async (req, res) => {
+app.get("/submittedAssignment/:email", logger, verifyToken, async (req, res) => {
   const result = await submittedAssignment.find({email: req.params.email}).toArray();
   res.send(result);
 });
@@ -103,7 +135,7 @@ app.delete("/submittedAssignment/:id", async (req, res) => {
 
 
 
-app.delete("/assignments/:email", async (req, res) => {
+app.delete("/assignments/:email",  async (req, res) => {
   const email = req.params.email;
   
   const query = { email: email };
@@ -111,7 +143,7 @@ app.delete("/assignments/:email", async (req, res) => {
   res.send(result);
 });
 
-app.put("/assignments/:id", async(req, res)=>{
+app.put("/assignments/:id",  async(req, res)=>{
   const id = req.params.id
   const filter= { _id: new ObjectId(id)}
   const options = {upsert: true};
@@ -137,19 +169,30 @@ app.put("/assignments/:id", async(req, res)=>{
 
 
 
-app.post("/jwt", async(req, res)=>{
-  const user = req.body;
-  console.log(user)
-  const token= jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, 
-    {expiresIn: '1h'})
-  res
-  .cookie('token', token, {
-    httpOnly: true,
-    secure: false, 
+
+
+
+
+app.post("/jwt", async (req, res) => {
+  try {
+    const user = req.body;
+    console.log(user);
+
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: '1h'
+    });
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: true, // Set to true if using HTTPS
+      sameSite: 'none' // For cross-site requests
+    }).send({ success: true });
+  } catch (error) {
+    console.error('Error generating JWT:', error);
+    res.status(500).send({ success: false, error: 'Internal server error' });
   }
-  )
-  .send({success: true})
- })
+});
+
 
 
 
